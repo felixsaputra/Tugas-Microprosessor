2340dc4fa8a2af151e3507b15425582ce37fa5d1
user29 <user29@user29.com>
2017-04-10 10:06:04 +0700

    M01_2016630001_02

---
+++ M01_2016630001_02.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+
+
+int main()
+{ char a,s,d,f;
+
+printf("input 1(angka/huruf) : ");
+scanf("%s",&a);
+
+printf("\ninput 2(angka/huruf) : ");
+scanf("%s",&s);
+
+printf("isi input 1 adalah %c dan input 2 adalah %c",a,s);
+printf("\nisi dari input 1 adalah %c dan input 2 adalah %c",);
+printf("\nisi dari input 1 adalah %c dan input 2 adalah %c",);
+
+return 0;
+}
