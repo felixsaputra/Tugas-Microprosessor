487404a00b9ab53a30adad0c017410158d04bafb
user29 <user29@user29.com>
2017-04-10 09:51:38 +0700

    M01_2016630012_02

---
+++ M01_2016630012_02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int* a;
+    int* b;
+    int c,d;
+
+    printf("Input 1 (Angka/Huruf)\t: ");
+    scanf("%d",&a);
+    printf("Input 2 (Angka/Huruf)\t: ");
+    scanf("%d",&b);
+
+    printf("\nIsi dari input 1 adalah %d dan input 2 adalah %d (Sebelum swap)\n",a,b);
+
+}
+
