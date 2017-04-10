b5c1c86b050cd98506d53e9ac12f80320adc36ae
user29 <user29@user29.com>
2017-04-10 09:53:15 +0700

    M01_2016630012_05

---
+++ M01_2016630012_05.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    char d;
+
+    printf("Nama Mahasiswa\t\t: ");
+    scanf("%s",&d);
+    printf("Banyak mata kuliaht\t: n\n");
+    printf("Nilai ke-n mahasiswa\t: ");
+    scanf("%d",&a);
+
+
+}
